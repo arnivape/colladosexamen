0e42cf3b1225277fa4d8f716394312505a21fc1b
Arnau <15584235.clot@fje.edu>
2020-04-17 17:28:21 +0200

    p02.sh

---
+++ p02.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+#######COMPROVANT SI L'ARXIU DE GUIÓ L'EXECUTA L'USUARI ROOT###################
+if (( EUID != 0 ))
+then
+  echo "Aquest script s'ha d'executar amb prilegis de l'usuari root"
+  exit 9 # He posat aquest número per assegurar-me que no coincideix amb cap al$
+fi
+#
+clear
+echo "Nom de la carpeta que crearem"
+read carpeta
+if[ carpeta = -d /var/ ];
+then echo "la carpeta ja existeix"
+else
+echo mkdir carpeta
+fi
+
+
